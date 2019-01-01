ed663b38cefe930414fe9c4ca1bba74671c21ba9
zef sowamn <fez_11@outlook.com>
2019-01-02 11:56:26 +1300

    Initial commit

---
+++ app/src/main/java/com/example/fez_1/eve_online_esi_tester/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fez_1.eve_online_esi_tester;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
